28269e1dbd4c58c0751902daff6c924c442f86c2
Lucas Antunes de Freitas <lucas.freitas.ext@brf.com>
2023-05-17 11:19:51 -0300

    feature/query_range_date - Adding query to range accidents for date

---
+++ src/connection-args/connection-args.spec.ts
@@ -0,0 +1,7 @@
+import { ConnectionArgs } from './connection-args';
+
+describe('ConnectionArgs', () => {
+  it('should be defined', () => {
+    expect(new ConnectionArgs()).toBeDefined();
+  });
+});
