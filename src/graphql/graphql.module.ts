10bda5472a424a090bdea7102e919e022b72ca47
Lucas Antunes de Freitas <lucas.freitas.ext@brf.com>
2023-05-17 15:02:42 -0300

    feature/deploy_vercel Change absolute path

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,12 +1,10 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      autoSchemaFile: join(__dirname, '../../temp/schema.gql'),
       driver: ApolloDriver,
     }),
   ],
