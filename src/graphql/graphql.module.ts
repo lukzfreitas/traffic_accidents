9126befaf454a7a51992022ee8f8333d3e08a8ee
Lucas Antunes de Freitas <lucas.freitas.ext@brf.com>
2023-07-11 15:24:32 -0300

    Add configuration cors local domain in configuration graphql

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,10 +1,14 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRoot({
+      cors: {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      },
       autoSchemaFile: true,
       playground: true,
       driver: ApolloDriver,
